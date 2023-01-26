a17ac15ef1069357c40654feae1826cae6477eb5
buchmayerlukas <lukasbuchmayer@gmail.com>
2023-01-26 18:12:54 +0100

    Initial commit

---
+++ src/main/java/com/example/slm_exam_buchmayer/SlmExamBuchmayerApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_exam_buchmayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmExamBuchmayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmExamBuchmayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slm_exam_buchmayer/SlmExamBuchmayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_exam_buchmayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmExamBuchmayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
